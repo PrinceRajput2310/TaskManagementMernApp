582c4bec60b042dfd4b43421de09058bd034eee3
Prince Kumar Singh <princekumarsingh@Princes-MacBook-Air.local>
2024-08-05 10:51:10 +0530

    changed api configuration

--- taskapp/src/utils/apiEndPoints.js
+++ taskapp/src/utils/apiEndPoints.js
@@ -1,20 +1,29 @@
-export const API_URL = "https://taskmanagement-rho.vercel.app";
+let apiSever = "http://localhost:3005";
 
-export const LOCALHOST_BACKEND_URL = "http://localhost:3005/api/v1";
+const frontendDomain = window.location.origin;
+
+if (frontendDomain.includes("localhost")) {
+  apiSever = "http://localhost:3005";
+} else if (frontendDomain.includes("taskmanagement")) {
+  apiSever = "https://taskmanagement-rho.vercel.app";
+}
+//   export const API_URL = "https://taskmanagement-rho.vercel.app";
+
+// export const LOCALHOST_BACKEND_URL = "http://localhost:3005";
 
 const API_ENDPOINT = {
-  login: `${API_URL}/api/v1/login`,
-  signup: `${API_URL}/api/v1/signup`,
-  logout: `${API_URL}/api/v1/logout`,
-  allTask: `${API_URL}/api/v1/todo/alltodo`,
-  newTask: `${API_URL}/api/v1/todo/new`,
-  updateTask: `${API_URL}/api/v1/todo/update`,
-  deleteTask: `${API_URL}/api/v1/todo/delete`,
-  searchTask: `${API_URL}/api/v1/todo/search`,
-  pendingTask: `${API_URL}/api/v1/todo/category?category=Pending`,
-  completedTask: `${API_URL}/api/v1/todo/category?category=Completed`,
-  taskDetail: `${API_URL}/api/v1/todo/taskdetail`,
-  allUsers: `${API_URL}/api/v1/allusers`,
+  login: `${apiSever}/api/v1/login`,
+  signup: `${apiSever}/api/v1/signup`,
+  logout: `${apiSever}/api/v1/logout`,
+  allTask: `${apiSever}/api/v1/todo/alltodo`,
+  newTask: `${apiSever}/api/v1/todo/new`,
+  updateTask: `${apiSever}/api/v1/todo/update`,
+  deleteTask: `${apiSever}/api/v1/todo/delete`,
+  searchTask: `${apiSever}/api/v1/todo/search`,
+  pendingTask: `${apiSever}/api/v1/todo/category?category=Pending`,
+  completedTask: `${apiSever}/api/v1/todo/category?category=Completed`,
+  taskDetail: `${apiSever}/api/v1/todo/taskdetail`,
+  allUsers: `${apiSever}/api/v1/allusers`,
 };
 
 export default API_ENDPOINT;
